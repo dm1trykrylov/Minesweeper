c5699913103d72e2809993127ded064469b51e61
Dmitry Krylov <231014d@gmail.com>
2022-04-22 21:57:26 +0400

    add settings, create frames

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from tkinter import *
-
+import settings
 
 root = Tk()
+root.configure(bg="black")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("Minesweeper")
+#root.resizable(False, False)
 
+
+top_frame = Frame(
+    root,
+    bg='red',
+    width=1440,
+    height=180
+)
+top_frame.place(x=0,y=188)
+
+left_frame = Frame(
+    root,
+    bg='blue',
+    width=1440,
+    height=180
+)
+top_frame.place(x=0,y=188)
+
+#Run the window
 root.mainloop()

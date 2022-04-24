a70b90d8be34edd25632c8703cecc14cba4f22c5
Dmitry Krylov <231014d@gmail.com>
2022-04-24 20:36:22 +0400

    Add surrounding mines logic

--- cell.py
+++ cell.py
@@ -26,10 +26,38 @@ class Cell:
     def onLeftClick(self, event):
         if self.is_mine:
             self.show_mine()
+        else:
+            self.show_cell()
 
     def show_mine(self):
         # A logic to show mine and interrupt the game
         self.btn_object.configure(bg='red')
+        self.btn_object.configure(state='disabled')
+
+    def get_cell_by_axes(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounding_cells(self):
+        cells = []
+        for x in range(self.x - 1, self.x + 2):
+            for y in range(self.y - 1, self.y + 2):
+                if x != self.x or y != self.y:
+                    cells.append(self.get_cell_by_axes(x, y))
+
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounding_mines_count(self):
+        count = len(
+            [cell for cell in self.surrounding_cells if cell.is_mine == True])
+        return count
+
+    def show_cell(self):
+        self.btn_object.configure(text=self.surrounding_mines_count)
 
     def onRightClick(self, event):
         print(event)
